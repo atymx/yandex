2110c6aa507abef8c271d9bbc56484f8c55ccbf7
Atyutskiy Maksim <macsim.1999@icloud.com>
2017-08-28 15:09:01 +0300

    functions for validations moved into new file

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 /**
  * Created by aty.max on 12.08.17.
  */
-
 'use strict';
 
 const validDomains = ['ya.ru', 'yandex.ru', 'yandex.by', 'yandex.ua', 'yandex.kz', 'yandex.com'];
 
-function validEmail(email) {
-    return /^\w+@\w+\.\w{2,4}$/i.test(email) && validDomains.indexOf(email.split("@")[1]) !== -1;
-}
-
-function validFio(fio) {
-    return fio.split(" ").length === 3;
-}
-
-function validPhone(phone) {
-    var sum = 0;
-    for (var i = 0; i < phone.length; i++) {
-        sum += parseInt(phone[i]);
-    }
-    return sum < 31;
-}
-
 $(document).ready(function () {
     // Маска для российских телефонных номеров
     $("input[name=phone]").inputmask('phoneru');
-});
-
-$("#submitButton").click(function () {
-    $("input[name=fio]").addClass("error");
 });
 
 $( '.js-input' ).keyup(function() {
@@ -37,4 +16,8 @@ $( '.js-input' ).keyup(function() {
     } else {
         $(this).removeClass('not-empty');
     }
+});
+
+$("#submitButton").click(function () {
+    //TODO: написать поведение формы
 });
